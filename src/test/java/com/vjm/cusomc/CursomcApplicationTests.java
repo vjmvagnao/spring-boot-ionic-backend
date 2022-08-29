eb7b355ad18eb3c1f38df287719ce164bcf366aa
Vagner <vjmvagnao@gmail.com>
2022-08-29 20:29:35 -0300

    Projeto criado

---
+++ src/test/java/com/vjm/cusomc/CursomcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vjm.cusomc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursomcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
