ea72c7a3c6462c62c619e8f186dd0c44a2da76a3
Vagner <vjmvagnao@gmai.com>
2022-08-31 07:34:38 -0300

    Criada a classe Categorias

--- src/main/java/com/vjm/cusomc/resources/CategoriaResource.java
+++ src/main/java/com/vjm/cusomc/resources/CategoriaResource.java
@@ -1,15 +1,26 @@
 package com.vjm.cusomc.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.vjm.cusomc.domain.Categoria;
 
 @RestController
 @RequestMapping(value="/categorias")
 public class CategoriaResource {
 	@RequestMapping(method=RequestMethod.GET)
-	public String listar(){
-		return "Rest ok!";
+	public List<Categoria> listar(){
+		
+		Categoria cat1 = new  Categoria(1, "Informatica");
+		Categoria cat2 = new  Categoria(2, "Escritorio");
+		List<Categoria> lista = new ArrayList<>();
+		lista.add(cat1);
+		lista.add(cat2);
+		return lista;
 		
 	}
 
