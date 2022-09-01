e879f7ff2f78f80886c1a85e4235e2165e2b1b24
Vagner <vjmvagnao@gmai.com>
2022-09-01 20:42:00 -0300

    Criando operacao de instanciacao

--- src/main/java/com/vjm/cusomc/CursomcApplication.java
+++ src/main/java/com/vjm/cusomc/CursomcApplication.java
@@ -1,13 +1,32 @@
 package com.vjm.cusomc;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.vjm.cusomc.domain.Categoria;
+import com.vjm.cusomc.repositories.CategariaRepository;
+
 @SpringBootApplication
-public class CursomcApplication {
+public class CursomcApplication implements CommandLineRunner {
+	
+	@Autowired
+	private CategariaRepository caregoriaRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Categoria cat1 = new Categoria (null, "Informatica");
+		Categoria cat2 = new Categoria (null, "Escritorio");
+		
+		caregoriaRepository.saveAll(Arrays.asList(cat1,cat2));
+		
 	}
 
 }
