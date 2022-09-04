a5f3e0b9acb4e9f4ec6ca9598009e1439accef97
Vagner <vjmvagnao@gmai.com>
2022-09-04 06:26:39 -0300

    Criadas as entidades Cidade e Estado

--- src/main/java/com/vjm/cusomc/CursomcApplication.java
+++ src/main/java/com/vjm/cusomc/CursomcApplication.java
@@ -8,8 +8,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.vjm.cusomc.domain.Categoria;
+import com.vjm.cusomc.domain.Cidade;
+import com.vjm.cusomc.domain.Estado;
 import com.vjm.cusomc.domain.Produto;
 import com.vjm.cusomc.repositories.CategariaRepository;
+import com.vjm.cusomc.repositories.CidadeRepository;
+import com.vjm.cusomc.repositories.EstadoRepository;
 import com.vjm.cusomc.repositories.ProdutoRepository;
 
 @SpringBootApplication
@@ -20,6 +24,12 @@ public class CursomcApplication implements CommandLineRunner {
 	
 	@Autowired
 	private ProdutoRepository produtoRepository;
+	
+	@Autowired
+	private EstadoRepository estadoRepository;
+	
+	@Autowired
+	private CidadeRepository cidadeRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
@@ -44,7 +54,18 @@ public class CursomcApplication implements CommandLineRunner {
 		caregoriaRepository.saveAll(Arrays.asList(cat1,cat2));
 		produtoRepository.saveAll(Arrays.asList(p1,p2,p3));
 		
+		Estado est1 = new Estado(null, "Minas Gerais");
+		Estado est2 = new Estado(null, "São Paulo");
 		
+		Cidade cid1 = new Cidade(null, "Uberlandia", est1);
+		Cidade cid2 = new Cidade(null, "São Paulo", est2);
+		Cidade cid3 = new Cidade(null, "Campinas", est2);
+				
+		est1.getCidades().addAll(Arrays.asList(cid1));		
+		est2.getCidades().addAll(Arrays.asList(cid2,cid3));
+		
+		estadoRepository.saveAll(Arrays.asList(est1,est2));
+		cidadeRepository.saveAll(Arrays.asList(cid1,cid2,cid3));
 		
 	}
 
---
+++ src/main/java/com/vjm/cusomc/repositories/EstadoRepository.java
@@ -0,0 +1,12 @@
+package com.vjm.cusomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.vjm.cusomc.domain.Estado;
+
+@Repository
+public interface EstadoRepository extends JpaRepository<Estado, Integer> {
+		 
+
+}
