1f6466d120f25ec6b4bd648cc5d7654a6a2b8644
Vagner <vjmvagnao@gmai.com>
2022-11-13 15:03:50 -0300

    Endpoint Pedido

--- src/main/java/com/vjm/cusomc/domain/ItemPedido.java
+++ src/main/java/com/vjm/cusomc/domain/ItemPedido.java
@@ -6,11 +6,14 @@ import java.util.Objects;
 import javax.persistence.EmbeddedId;
 import javax.persistence.Entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 
 @Entity
 public class ItemPedido implements Serializable {	
 	private static final long serialVersionUID = 1L;
 	
+	@JsonIgnore
 	@EmbeddedId
 	private ItemPedidoPK id = new ItemPedidoPK();
 	private Double desconto;
@@ -28,10 +31,11 @@ public class ItemPedido implements Serializable {
 		this.preco = preco;
 	}
 	
+	@JsonIgnore
 	public Pedido getPedido() {
 		return id.getPedido();
 	}
-	
+		
 	public Produto getProduto() {
 		return id.getProduto();
 	}
---
+++ src/main/java/com/vjm/cusomc/services/PedidoService.java
@@ -0,0 +1,24 @@
+package com.vjm.cusomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.vjm.cusomc.domain.Pedido;
+import com.vjm.cusomc.exceptions.ObjectNotFoundException;
+import com.vjm.cusomc.repositories.PedidoRepository;
+
+@Service
+public class PedidoService {
+	
+	@Autowired
+	private PedidoRepository repo;
+	
+	public Pedido buscar(Integer id) {
+		Optional<Pedido> obj = repo.findById(id);		
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				 "Objeto não encontrado! Id: " + id + ", Tipo: " + Pedido.class.getName()));		
+	}
+
+}
