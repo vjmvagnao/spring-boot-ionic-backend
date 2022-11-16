44f32b8210370a48e91cf64207dceb4f80683177
Vagner <vjmvagnao@gmai.com>
2022-11-16 20:50:15 -0300

    Insert Categoria

--- src/main/java/com/vjm/cusomc/resources/CategoriaResource.java
+++ src/main/java/com/vjm/cusomc/resources/CategoriaResource.java
@@ -1,11 +1,15 @@
 package com.vjm.cusomc.resources;
 
+import java.net.URI;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.vjm.cusomc.domain.Categoria;
 import com.vjm.cusomc.services.CategoriaService;
@@ -24,5 +28,15 @@ public class CategoriaResource {
 		return ResponseEntity.ok().body(obj);
 		
 	}
+	
+	@RequestMapping(method=RequestMethod.POST)
+	public ResponseEntity<Void> insert(@RequestBody Categoria obj){
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+				.path("/{id}").buildAndExpand(obj.getId()).toUri();
+		
+		return ResponseEntity.created(uri).build();		
+	}
+
 
 }
